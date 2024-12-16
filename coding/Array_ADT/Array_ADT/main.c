afb1c75de57476f378c88804203d4eb2d005be14
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2024-12-16 20:52:42 +0530

    ADT

--- coding/Array_ADT/Array_ADT/main.c
+++ coding/Array_ADT/Array_ADT/main.c
@@ -1,14 +1,51 @@
-//
-//  main.c
-//  Array_ADT
-//
-//  Created by Akshay Chandra on 16/12/24.
-//
-
+#include <stdlib.h>
 #include <stdio.h>
 
+struct Array
+{
+    int A[10];
+    int length;
+    int size;
+};
+
+void display(struct Array A)
+{
+    for(int i=0;i<A.length;i++)
+    {
+        printf("%d ",A.A[i]);
+    }
+}
+
+void append(struct Array *Arr,int value)
+{
+    if(Arr->length<Arr->size)
+    {
+        Arr->A[Arr->length++]=value;
+    }
+}
+
+void insert(struct Array *Arr, int index, int value)
+{
+    if(index >= 0 && index <= Arr->length)
+    {
+        for(int i=Arr->length;i>index;i--)
+        {
+            Arr->A[i]=Arr->A[i-1];
+            
+        }
+        Arr->A[index]=value;
+        Arr->length++;
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    struct Array A1 = {{2,4,3,2},4,10};
+    //append(A1,3,22);
+    //append(&A1,66);
+    insert(&A1, 3, 23);         //OP: 2 4 3 23 2
+    append(&A1,66);             //OP: 2 4 3 23 2 66
+    insert(&A1,7 , 23);         //OP: invalid, as the length of array is small
+    display(A1);
+    
     return 0;
 }
