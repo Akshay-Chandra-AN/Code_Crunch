b16478ec28176e2a83266becb11355f47393abca
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2024-12-16 20:49:37 +0530

    Array ADT

---
+++ coding/Array_ADT/Array_ADT/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Array_ADT
+//
+//  Created by Akshay Chandra on 16/12/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
