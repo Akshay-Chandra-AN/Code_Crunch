b62b33a7870d37450f2e4f903bdfc2a588a19712
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2025-02-23 10:04:40 +0530

    Revise_2025_feb

---
+++ coding/pointers/pointers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pointers
+//
+//  Created by Akshay Chandra on 23/02/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
