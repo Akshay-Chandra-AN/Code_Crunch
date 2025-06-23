b507be2cc42f00790b84673d8eeb89d159a67308
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2025-06-23 09:13:02 +0530

    ex 3 and 4 recursion

--- coding/Recursion-FUNCTION/Recursion-FUNCTION/main.c
+++ coding/Recursion-FUNCTION/Recursion-FUNCTION/main.c
@@ -4,14 +4,37 @@ void fun1(int n)
 {
     if(n>0)
     {
-        printf("%d ",n);
+        printf("%d \n",n);
         fun1(n-1);
     }
 }
 
 
+int fun2 (int n)
+{
+    if(n>0)
+    {
+        return fun2(n-1)+n;
+    }
+    return 0;
+}
+
+int fun3 (int n)
+{
+    static int x=0;
+    if(n>0)
+    {
+        x++;
+        return fun3(n-1)+x;
+    }
+    return 0;
+}
+
+
 int main(int argc, const char * argv[]) {
     int x=3;
-    fun1(x);
+    // fun1(x);                 //example 1
+    //printf("%d \n",fun2(5));       //example 3
+    printf("%d \n",fun3(5));       //example 4
     return 0;
 }
