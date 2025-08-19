96190fa1525f2548c816f5ce7eb852d0b08ae953
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2025-08-19 22:38:27 +0530

    traverse list

--- coding/pyajama_Linkeldlist/pyajama_Linkeldlist/main.c
+++ coding/pyajama_Linkeldlist/pyajama_Linkeldlist/main.c
@@ -13,22 +13,30 @@ int init(char *data)
 {
     printf("%p\n", head);   //memory is not allocated hence if i print, head = 0
     
+    if(head)    //if head is initialized already then return error code., as we need not initialize it again
+    {
+        printf("list already initialized");
+        return -1;
+    }
+    
     //we want malloc to allocate some memory for my head
     //how much size? whatever the size of struct node3
     printf("sizeof(node)%ld\n",sizeof(node));
     
     head = malloc (sizeof(node));
     
-    if(head == NULL)
+    if (!head)      // !head is same as if(head == NULL) on linux
     {
         printf("memory allocation failed");
-        return -1;
+        return -2;      //if head is initialized already then return error code.
     }
     
     printf("%p\n",head);    //mem is allocated, we expect a valid address
     
-    //now because it is the first element, the next node needs to be point to null
+    //now because it is the first element, the next node needs to be point to null, because there
+    //is nothing to point to right now
     head->next = NULL;
+    //now copy the data
     memcpy(head->data, data, strlen(data));
     
     //now print, if everything is working as expected
@@ -38,11 +46,43 @@ int init(char *data)
     return 0;
 }
 
+int add(char *data)
+{
+    if(!data) //make sure data is not null, if it is then no point in adding that as a node
+    {
+        printf("inalid data\n");
+        return -1;
+    }
+    
+    node *n = malloc(sizeof(node));
+    if(!n) //if memory is allocated then it is not null
+    {
+        printf("allocation failed\n");
+        return -2;
+    }
+    
+    n->next = head;
+    memcpy(n->data, data, strlen(data));
+    head = n;
+    
+    return 0;
+}
+
+void traverse(void)
+{
+    for(node *cursor = head;
+        cursor != NULL;
+        cursor = cursor->next)
+    {
+        printf("list : %s\n", cursor->data);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    init("hello");
-    
-    
+    init("dello");
+    add("akshay");
+    add("chandra");
+    traverse();
     
     
     return 0;
