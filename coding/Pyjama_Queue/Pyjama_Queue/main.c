8b1506e9217bd888f585fe81ad225813d1619b4f
Akshay-Chandra-AN <79188105+Akshay-Chandra-AN@users.noreply.github.com>
2025-09-03 20:00:40 +0530

    queue improved and fetch

--- coding/Pyjama_Queue/Pyjama_Queue/main.c
+++ coding/Pyjama_Queue/Pyjama_Queue/main.c
@@ -8,12 +8,14 @@ typedef struct node
 }node;
 
 node *head;
+node *tail;
 
 void init(int adata)
 {
     head = malloc(sizeof(node));
     head->next = NULL;
     head->data = adata;
+    tail = head;
 }
 
 int add_end(int bdata)
@@ -35,6 +37,35 @@ int add_end(int bdata)
     return 0;
 }
 
+int queue(int bdata)
+{
+    node *n;
+    n = malloc(sizeof(node));
+    
+    n->data= bdata;
+    n->next = NULL;
+    
+    tail->next = n;
+    tail = n;
+    return 0;
+}
+
+int fetch()
+{
+    if(!head)
+    {
+        return -1;
+    }
+    
+    int first;
+    node *delete;
+    first = head->data;
+    delete = head;
+    head = head->next;
+    free(delete);
+    printf("%d ", first);
+    return 0;
+}
 
 void traverse(void)
 {
@@ -42,14 +73,18 @@ void traverse(void)
         cursor != NULL;
         cursor = cursor->next)
     {
-        printf("%d",cursor->data);
+        printf("traversal : %d ",cursor->data);
     }
 }
 
 int main(int argc, const char * argv[]) {
     init(10);
-    add_end(55);
-    add_end(552);
+    queue(55);
+    queue(552);
+    traverse();
+    fetch();
+    fetch();
+  
     traverse();
     return 0;
 }
